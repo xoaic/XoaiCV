89a78c3772e7b568774c887c0496e7a86af2e5ac
xoaic <xoaic@xcc.one>
2023-01-14 19:41:31 +0700

    initialize ver 1.2

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-let list = document.querySelectorAll('#nav ul li')
-function activeLink() {
-    list.forEach((item) => item.classList.remove('active'))
-    this.classList.add('active')
-}
-list.forEach((item) => item.addEventListener('click', activeLink))
+const sections = document.querySelectorAll("section");
+let currentSection = 0;
+let scrollPosition = 0;
+
+window.addEventListener("wheel", function(event){
+    if (event.deltaY > 0 && currentSection < sections.length - 1) {
+        currentSection++;
+        scrollPosition = sections[currentSection].offsetTop;
+        window.scrollTo({
+            top: scrollPosition,
+            behavior: "smooth"
+        });
+    } else if (event.deltaY < 0 && currentSection > 0) {
+        currentSection--;
+        scrollPosition = sections[currentSection].offsetTop;
+        window.scrollTo({
+            top: scrollPosition,
+            behavior: "smooth"
+        });
+    }
+    //Checking if user try to use scrollbar
+    setTimeout(function() {
+        if (window.scrollY !== scrollPosition) {
+            window.scrollTo({
+                top: scrollPosition,
+                behavior: "smooth"
+            });
+        }
+    }, 500);
+});
