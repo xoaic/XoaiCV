61640fdd1c74710ff5f31e282db0ac78427a73d4
xoaic <xoaic@xcc.one>
2023-01-17 01:57:53 +0700

    initialize ver 1.2

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 const sections = document.querySelectorAll("section");
+const name1 = document.getElementById("nickname");
+const name2 = document.getElementById("name");
 let currentSection = 0;
 let scrollPosition = 0;
+
+// function toggleNames() {
+//     name1.classList.toggle("hidden");
+//     name2.classList.toggle("hidden");
+// }
+//
+// setInterval(toggleNames, 1500);
+
+const barBtns = document.querySelectorAll(".bar-btn");
+
+barBtns.forEach(function(barBtn) {
+    barBtn.addEventListener("click", function() {
+        barBtns.forEach(function(btn) {
+            btn.classList.remove("bar-btn-active");
+        });
+        this.classList.add("bar-btn-active");
+    });
+});
+
+
+function viewTab(type) {
+    const thisType = document.getElementById(type);
+    const types = document.getElementsByClassName("proj-type");
+    for (let i = 0; i < types.length; i++) {
+        types[i].classList.add("hidden");
+    }
+    thisType.classList.remove("hidden");
+}
 
 window.addEventListener("wheel", function(event){
     if (event.deltaY > 0 && currentSection < sections.length - 1) {
@@ -18,7 +48,7 @@ window.addEventListener("wheel", function(event){
             behavior: "smooth"
         });
     }
-    //Checking if user try to use scrollbar
+
     setTimeout(function() {
         if (window.scrollY !== scrollPosition) {
             window.scrollTo({
